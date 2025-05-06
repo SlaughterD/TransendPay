4b6291460b50a81f7f0428c131a858d77beaf29a
SlaughterD <davidgithinji09@gmail.com>
2025-05-06 20:23:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from app.routes import auth, kyc, transactions, crypto
 
 app = FastAPI()
 
-app.include_router(auth.router)
-app.include_router(kyc.router)
-app.include_router(transactions.router)
-app.include_router(crypto.router)
-
 @app.get("/")
-def root():
-    return {"msg": "Welcome to TransendPay API"}
+def read_root():
+    return {"message": "TransendPay API is running"}
